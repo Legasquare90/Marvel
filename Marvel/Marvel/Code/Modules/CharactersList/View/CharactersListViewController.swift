dc56fd9a193b3db565cb7ec50642578c48280589
Jose Angel Cuadrado Mingo <jose.cuadrado@wegow.es>
2020-07-09 19:10:04 +0200

    Add design listing characters

--- Marvel/Marvel/Code/Modules/CharactersList/View/CharactersListViewController.swift
+++ Marvel/Marvel/Code/Modules/CharactersList/View/CharactersListViewController.swift
@@ -9,16 +9,41 @@ import UIKit
 
 class CharactersListViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    // MARK: - Architecture
+    
     private lazy var presenter: CharactersListPresenterInput = self.makePresenter()
 
     private func makePresenter() -> CharactersListPresenterInput {
         return CharactersListPresenter(viewInterface: self)
     }
     
+    // MARK: - Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(UINib(nibName: "CharacterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CharacterCollectionViewCell")
     }
 
+}
+
+extension CharactersListViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharacterCollectionViewCell", for: indexPath) as! CharacterCollectionViewCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 120.0, height: 120.0)
+    }
 }
 
 extension CharactersListViewController: CharactersListPresenterOutput {
---
+++ Marvel/Marvel/Code/Views/Cells/CharacterCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  CharacterCollectionViewCell.swift
+//  Marvel
+//
+//  Created by Jose Angel Cuadrado Mingo on 09/07/2020.
+//
+
+import UIKit
+import Kingfisher
+
+struct CharacterCellDesign {
+    var name: String
+    var imageURL: String
+
+    init(name: String = "", imageURL: String = "") {
+        self.name = name
+        self.imageURL = imageURL
+    }
+}
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var characterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.characterImageView.layer.cornerRadius = self.characterImageView.frame.size.height / 2
+    }
+    
+    func setupCell(design: CharacterCellDesign) {
+        let url = URL(string: design.imageURL)
+        characterImageView.kf.setImage(with: url)
+        nameLabel.text = design.name
+    }
+
+}
