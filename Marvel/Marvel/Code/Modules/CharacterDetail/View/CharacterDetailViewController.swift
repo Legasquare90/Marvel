6fbdfe28408a5c517de2674c4cd3dec35cf5636c
Jose Angel Cuadrado Mingo <jose.cuadrado@wegow.es>
2020-07-12 18:07:05 +0200

    Show collections in table

--- Marvel/Marvel/Code/Modules/CharacterDetail/View/CharacterDetailViewController.swift
+++ Marvel/Marvel/Code/Modules/CharacterDetail/View/CharacterDetailViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 import Kingfisher
 
+enum TableSection: Int {
+    case Comics = 0
+    case Stories
+    case Events
+    case Series
+}
+
 class CharacterDetailViewController: BaseViewController {
 
     @IBOutlet weak var characterImageView: UIImageView!
@@ -23,6 +30,8 @@ class CharacterDetailViewController: BaseViewController {
     @IBOutlet weak var wikiLinkLabel: UILabel!
     
     var character: Character!
+    
+    var sectionSelected: TableSection = .Comics
 
     // MARK: - Architecture
     
@@ -41,6 +50,9 @@ class CharacterDetailViewController: BaseViewController {
                 
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.layoutMargins = UIEdgeInsets.zero
+        tableView.separatorInset = UIEdgeInsets.zero
+        tableView.tableFooterView = UIView()
         
         setLocalizableStrings()
         setCharacterData()
@@ -82,7 +94,11 @@ class CharacterDetailViewController: BaseViewController {
     }
     
     @IBAction func collectionsSegmentedControlDidChange(_ sender: Any) {
-        
+        self.sectionSelected = TableSection(rawValue: collectionsSegmentedControl.selectedSegmentIndex) ?? .Comics
+        self.tableView.reloadData()
+        if self.tableView.numberOfRows(inSection: 0) > 0 {
+            self.tableView.scrollToRow(at: IndexPath (item: 0, section: 0), at: UITableView.ScrollPosition.top, animated: false)
+        }
     }
     
     // MARK: - Other methods
@@ -133,11 +149,36 @@ class CharacterDetailViewController: BaseViewController {
 
 extension CharacterDetailViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        switch self.sectionSelected {
+        case .Comics:
+            return character.comics?.items?.count ?? 0
+        case .Stories:
+            return character.stories?.items?.count ?? 0
+        case .Events:
+            return character.events?.items?.count ?? 0
+        case .Series:
+            return character.series?.items?.count ?? 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        var name = ""
+        switch self.sectionSelected {
+        case .Comics:
+            name = character.comics?.items?[indexPath.row].name ?? ""
+        case .Stories:
+            name = character.stories?.items?[indexPath.row].name ?? ""
+        case .Events:
+            name = character.events?.items?[indexPath.row].name ?? ""
+        case .Series:
+            name = character.series?.items?[indexPath.row].name ?? ""
+        }
+
+        let basicCell = UITableViewCell()
+        basicCell.textLabel?.text = name
+        basicCell.textLabel?.font = UIFont.systemFont(ofSize: 14.0, weight: .medium)
+        basicCell.textLabel?.numberOfLines = 2
+        return basicCell
     }
 }
 
